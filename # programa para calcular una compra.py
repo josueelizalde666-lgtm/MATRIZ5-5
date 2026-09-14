043961b10a675a3a35c8067ba714ea4224a073d0
JOSUE ALEXANDER ELIZALDE <josueelizalde666@gmail.com>
2026-09-13 19:21:44 -0500

    Add files via upload

---
+++ # programa para calcular una compra.py
@@ -0,0 +1,10 @@
+# programa para calcular una compra
+def calcular_total(precio,cantidad):
+
+
+  total = precio * cantidad  
+  return total
+precio=10
+cantidad=3
+resultado= calcular_total(precio,cantidad) 
+print(resultado) 
